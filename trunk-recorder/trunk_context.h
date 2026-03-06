c96a26db04947153850639217992768e6700355d
Nathan Howell <nathan.d.howell@gmail.com>
2026-03-06 13:07:17 -0800

    Remove dead setup_systems and calls vector from TrunkContext
    Conventional channel creation is now driven by Rust via
    tr_create_conventional_call. The C++ setup_systems code and the
    calls vector it populated are no longer used.

--- trunk-recorder/trunk_context.h
+++ trunk-recorder/trunk_context.h
@@ -10,12 +10,10 @@
 
 class Source;
 class System;
-class Call;
 
 struct TrunkContext {
   std::vector<std::shared_ptr<Source>> sources;
   std::vector<std::shared_ptr<System>> systems;
-  std::vector<std::shared_ptr<Call>> calls;
   gr::top_block_sptr tb;
   Config config;
 };
